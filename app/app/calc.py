04770767573701a49655f36179cac5681f6f357b
Haezal Musa <haezal@um.edu.my>
2023-04-11 12:39:49 +0800

    Fix bug flake8

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator function
 """
 
+
 def add(x, y):
-    """Add x and y and return the result"""
+    """Add x and y and return the result."""
     return x + y
 
+
 def substract(x, y):
-    """Substract x and y and return the result"""
+    """Substract x and y and return the result."""
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -5,14 +5,14 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test calculation functions"""
-
 
     def test_add_numbers(self):
         """Test that values are added together"""
         res = calc.add(5, 6)
-        self.assertEqual(res, 11) 
+        self.assertEqual(res, 11)
 
     def test_substract_numbers(self):
         """Test substracting numbers"""
